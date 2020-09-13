8dc8ec346823226cbb9a6cc122e4f39d8685a038
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2020-09-13 00:10:40 -0400

    Add group options, add encryption mode

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from optparse import OptionParser
+from optparse import OptionParser, OptionGroup
 
 from dalsp_crypt import dalsp_decrypt
 from dalsp_crypt import dalsp_encrypt
@@ -8,34 +8,42 @@ from dalsp_crypt import dalsp_encrypt
 def main():
     print("Date A Live: Spirit Pledge Assets Decryption tool\n")
     print("Decrypt all assets in source directory and save in destination directory\n")
-    parser = OptionParser()
+    usage = "usage: %prog -i INPUT -o OUTPUT [options]"
+    parser = OptionParser(usage=usage)
     parser.add_option("-i", "--input", dest="input_path",
                       help="Encrypted Data Source (folder/file)", metavar="\"INPUT\"")
     parser.add_option("-o", "--output", dest="output_path",
                       help="Decrypted Data Destination (MUST BE A FOLDER)", metavar="\"OUTPUT\"")
     parser.add_option("-v", "--verbose",
                       action="store_true", dest="verbose", default=False,
-                      help="Print debug messages to debug.log")
-    parser.add_option("-u", "--unpack",
-                      action="store_true", dest="unpackPVR", default=False,
-                      help="Unpack PVR assets")
-    parser.add_option("-k", "--keepPVR",
-                      action="store_true", dest="keepPVR", default=False,
-                      help="Keep PVR assets after unpack")
-    parser.add_option("-w", "--overwrite",
-                      action="store_true", dest="overwrite", default=False,
-                      help="Overwrite assets even if decrypted assets exist at destination")
-    parser.add_option("-e", "--encryption", action="store_true", dest="encryption", default=False,
-                      help="Enable encryption mode")
-    parser.add_option("-em", "--mode", dest="encrypt_mode",
-                      help="Encryption mode (ZIP,LZ4,PCM)", metavar="\"MODE\"")
+                      help="Print debugging messages to debug.log")
+    decryption = OptionGroup(parser, "Decryption Options",
+                             "Some useful decryption options.")
+    decryption.add_option("-u", "--unpack",
+                          action="store_true", dest="unpackPVR", default=False,
+                          help="Unpack PVR assets (Recommended)")
+    decryption.add_option("-k", "--keepPVR",
+                          action="store_true", dest="keepPVR", default=False,
+                          help="Keep PVR assets after unpack (Recommended for debugging)")
+    decryption.add_option("-w", "--overwrite",
+                          action="store_true", dest="overwrite", default=False,
+                          help="Overwrite assets even if decrypted assets exist at destination")
+    parser.add_option_group(decryption)
 
+    encryption = OptionGroup(parser, "Encryption Options",
+                             "Currently in testing.\nNOTE: For encryption mode PCM, you can explicitly set hex XOR value (4th byte of the original file) by \"-m pcm,<xor_hex>\" to produce identical result.\nHowever, this is not needed in the whole decryption scheme in the game")
+    encryption.add_option("-e", "--encryption", action="store_true", dest="encryption", default=False,
+                          help="Enable encryption mode")
+    encryption.add_option("-m", "--mode", dest="encrypt_mode",
+                          help="Encryption mode (ZIP,LZ4,PCM), case-insensitive", metavar="\"MODE\"")
+    parser.add_option_group(encryption)
 
     (options, args) = parser.parse_args()
 
     if options.input_path is None or options.output_path is None:
         print()
-        print("No input or output path specified. Run \"python main.py -h\" for more details")
+        print(
+            "No input or output path specified. Run \"python main.py -h\" for more details")
         print()
         return
     if not options.encryption:
@@ -48,11 +56,16 @@ def main():
         if os.path.exists(plistout):
             os.remove(plistout)
     else:
-        encrypt = dalsp_encrypt.DateALive_encryption(options)
-        if os.path.isfile(options.input_path):
-            encrypt.crypt_single_file()
+        if options.encrypt_mode is not None:
+            encrypt = dalsp_encrypt.DateALive_encryption(options)
+            if os.path.isfile(options.input_path):
+                encrypt.crypt_single_file()
+            else:
+                encrypt.crypt_folder()
         else:
-            encrypt.crypt_folder()
+            print(
+                "Need to use \"-m\" with specified encryption modes: ZIP,LZ4 or PCM. Run \"python main.py -h\" for more details")
+            return
 
 
 if __name__ == "__main__":
