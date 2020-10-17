0b79a90f7021bb7c59fac509b4cdcd8dd8eb3f87
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2020-10-17 11:23:41 -0400

    draft for iOS PVRTC

--- draft.py
+++ draft.py
@@ -2,12 +2,13 @@ from PIL import Image
 import struct
 from tex2img import basisu_decompress 
 
-with open("tmp/icon-gift/alpha_531009.pvr","rb") as f:
+with open("tmp/city_12701.pvr","rb") as f:
     buff = f.read()
     pixel_format = struct.unpack('<I', buff[4*2:4*2+4])[0]
-    print(pixel_format)
     if pixel_format == 6:
         pixel_format = 0
+    elif pixel_format == 2:
+        pixel_format = 11
     height = struct.unpack('<I', buff[4*6:4*6+4])[0]
     width = struct.unpack('<I', buff[4*7:4*7+4])[0]
     img=buff[67:]
