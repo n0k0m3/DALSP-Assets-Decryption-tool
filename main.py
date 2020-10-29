49b6649a83308c312455f152ade9ced4f55d1c63
n0k0m3 <vmnguyen251@gmail.com>
2020-10-29 00:59:26 -0400

    Upgrade from optparse to argparse (#3)
    * Required Arguments
    
    * Upgrade from optparse to argparse
    
    * pep8 formatting
    
    * some bug fixes
    
    * Edit README

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from optparse import OptionParser, OptionGroup
+from argparse import ArgumentParser
 
 from dalsp_crypt import dalsp_decrypt
 from dalsp_crypt import dalsp_encrypt
@@ -7,35 +7,35 @@ from dalsp_crypt import dalsp_encrypt
 
 def main():
     print("Date A Live: Spirit Pledge Assets Decryption tool\n")
-    print("Decrypt all assets in source directory and save in destination directory\n")
-    usage = "usage: %prog -i INPUT -o OUTPUT [options]"
-    parser = OptionParser(usage=usage)
-    parser.add_option("-i", "--input", dest="input_path",
-                      help="Encrypted Data Source (folder/file)", metavar="\"INPUT\"")
-    parser.add_option("-o", "--output", dest="output_path",
-                      help="Decrypted Data Destination (MUST BE A FOLDER)", metavar="\"OUTPUT\"")
-    parser.add_option("-v", "--verbose",
-                      action="store_true", dest="verbose", default=False,
-                      help="Print debugging messages to debug.log")
-    decryption = OptionGroup(parser, "Decryption Options",
-                             "Some useful decryption options.")
-    decryption.add_option("-k", "--keepPVR",
-                          action="store_true", dest="keepPVR", default=False,
-                          help="Keep PVR assets after unpack (Recommended for debugging)")
-    decryption.add_option("-w", "--overwrite",
-                          action="store_true", dest="overwrite", default=False,
-                          help="Overwrite assets even if decrypted assets exist at destination")
-    parser.add_option_group(decryption)
+    parser = ArgumentParser(description='Decrypt all assets in source directory and save in destination directory')
+    parser.add_argument(dest="input_path",
+                        help="Encrypted Data Source (folder/file)", metavar="INPUT")
+    parser.add_argument(dest="output_path",
+                        help="Decrypted Data Destination (MUST BE A FOLDER)", metavar="OUTPUT")
+    parser.add_argument("-v", "--verbose",
+                        action="store_true", dest="verbose", default=False,
+                        help="Print debugging messages to debug.log")
+    decryption = parser.add_argument_group(title="decryption options",
+                                           description="Some useful decryption options.")
+    decryption.add_argument("-k", "--keepPVR",
+                            action="store_true", dest="keepPVR", default=False,
+                            help="Keep PVR assets after unpack (Recommended for debugging)")
+    decryption.add_argument("-w", "--overwrite",
+                            action="store_true", dest="overwrite", default=False,
+                            help="Overwrite assets even if decrypted assets exist at destination")
 
-    encryption = OptionGroup(parser, "Encryption Options",
-                             "Currently in testing.\nNOTE: For encryption mode PCM, you can explicitly set hex XOR value (4th byte of the original file) by \"-m pcm,<xor_hex>\" to produce identical result.\nHowever, this is not needed in the whole decryption scheme in the game")
-    encryption.add_option("-e", "--encryption", action="store_true", dest="encryption", default=False,
-                          help="Enable encryption mode")
-    encryption.add_option("-m", "--mode", dest="encrypt_mode",
-                          help="Encryption mode (ZIP,LZ4,PCM), case-insensitive", metavar="\"MODE\"")
-    parser.add_option_group(encryption)
+    encryption = parser.add_argument_group(title="encryption options",
+                                           description="Currently in testing.\nNOTE: For encryption mode PCM, you can "
+                                                       "explicitly set hex XOR value (4th byte of the original file) "
+                                                       "by \"-m pcm,<xor_hex>\" to produce identical "
+                                                       "result.\nHowever, this is not needed in the whole decryption "
+                                                       "scheme in the game")
+    encryption.add_argument("-e", "--encryption", action="store_true", dest="encryption", default=False,
+                            help="Enable encryption mode")
+    encryption.add_argument("-m", "--mode", dest="encrypt_mode",
+                            help="Encryption mode (ZIP,LZ4,PCM)", metavar="MODE")
 
-    (options, args) = parser.parse_args()
+    options = parser.parse_args()
 
     if options.input_path is None or options.output_path is None:
         print()
@@ -58,7 +58,8 @@ def main():
                 encrypt.crypt_folder()
         else:
             print(
-                "Need to use \"-m\" with specified encryption modes: ZIP,LZ4 or PCM. Run \"python main.py -h\" for more details")
+                "Need to use \"-m\" with specified encryption modes: ZIP,LZ4 or PCM. Run \"python main.py -h\" for "
+                "more details")
             return
 
 
